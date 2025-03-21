7fc74afa6f15b11456dd77b7f500b5b7ad9380ce
Admin <nguyentktk00218@fpt.edu.vn>
2025-03-21 16:20:10 +0700

    Tạo Mới

---
+++ TigerSliver/src/main/java/com/tigersliver/tigersliver/TigerSliver.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tigersliver.tigersliver;
+
+/**
+ *
+ * @author Admin
+ */
+public class TigerSliver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
